9cfde6723b512b63becc481a8cfa96cd3ca34e2a
njmbb8 <nmcgr001@gmail.com>
2021-10-08 15:28:43 -0400

    commit tree 574d0acfe4322c1e8264db087abf958cab97cd30

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == '.'
   end
 
   def question?
-
+    self[-1] == '?'
   end
 
   def exclamation?
-
+    self[-1] == '!'
   end
 
   def count_sentences
-
+    binding.pry
+    self.scan(/(^|\s).+?[.!?]{1}/s).size
   end
 end
