1e0dc20c35d937e3dc291b925f094e7e4ea1c994
njmbb8 <nmcgr001@gmail.com>
2021-10-08 15:29:09 -0400

    Completed Assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == '.'
   end
 
   def question?
-
+    self[-1] == '?'
   end
 
   def exclamation?
-
+    self[-1] == '!'
   end
 
   def count_sentences
-
+    self.scan(/(^|\s).+?[.!?]{1}/s).size
   end
 end
